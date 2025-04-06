b96219ff043ba0731cdd2eba0196d2c82b7e17a9
Amirhossein Alian <alianmail81@gmail.com>
2025-04-06 17:18:47 +0330

    adding code for getting publci repos information

--- src/main/kotlin/RetrofitHelper.kt
+++ src/main/kotlin/RetrofitHelper.kt
@@ -1,21 +1,27 @@
 package edu.sharif.kotlin
 
-class RetrofitHelper {
-}
-// 📁 network/RetrofitClient.kt
-package ir.sharif.retrofit_14.network
-
+import edu.sharif.kotlin.data.api.GitHubService
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-object RetrofitClient {
-    private const val BASE_URL = "https://api.github.com/"
+// Singleton Design Pattern
+object RetrofitHelper {
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://api.github.com")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    val gson: Gson by lazy {
+        GsonBuilder()
+            .setPrettyPrinting()
+            .create()
+    }
 
     val githubService: GitHubService by lazy {
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(GitHubService::class.java)
+        retrofit.create(GitHubService::class.java)
     }
 }
--- src/main/kotlin/data/api/GitHubService.kt
+++ src/main/kotlin/data/api/GitHubService.kt
@@ -1,12 +1,18 @@
 package edu.sharif.kotlin.data.api
 
 import edu.sharif.kotlin.data.model.GitHubUser
+import edu.sharif.kotlin.data.model.GitHubRepo
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GitHubService {
-    @GET("users/{username}/repos")
-    suspend fun getUser(
+    @GET("users/{username}")
+    suspend fun getUserInfo(
         @Path("username") username: String
     ): GitHubUser
+
+    @GET("users/{username}/repos")
+    suspend fun getUserRepositories(
+        @Path("username") username: String
+    ): List<GitHubRepo>
 }
