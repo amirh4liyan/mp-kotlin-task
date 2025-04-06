83bd02557c55bb5ffa21affefc6519c63178d683
Amirhossein Alian <alianmail81@gmail.com>
2025-04-06 17:10:39 +0330

    needed dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.21"
 }
 
-group = "org.example"
+group = "edu.sharif"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,6 +10,13 @@ repositories {
 }
 
 dependencies {
+    // Coroutines Lib
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+    // Retrofit Lib
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
     testImplementation(kotlin("test"))
 }
 
