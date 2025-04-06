2385005cf2e8fd3d01d20dddb6d441008d901198
Amirhossein Alian <alianmail81@gmail.com>
2025-04-06 16:05:21 +0330

    adding initial API Interface and Data Class

--- src/main/kotlin/GitHubService.kt
+++ src/main/kotlin/GitHubService.kt
@@ -1,4 +1,11 @@
 package edu.sharif.kotlin
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface GitHubService {
+    @GET("users/{username}/repos")
+    suspend fun getUserInfo(
+        @Path("username") username: String
+    ): List<GitHubUser>
 }
