ba94d9d4a7a0e8b0ea4f0a772ecd67623d3257bd
Amirhossein Alian <alianmail81@gmail.com>
2025-04-06 16:07:02 +0330

    moved to data

--- src/main/kotlin/GitHubUser.kt
+++
@@ -1,9 +0,0 @@
-package edu.sharif.kotlin
-
-data class GitHubUser(
-    val user: String,
-    val publicRepos: MutableList<String>,
-    val JoinedDate: String,
-    val followersCount: Int,
-    val followingCount: Int
-)
